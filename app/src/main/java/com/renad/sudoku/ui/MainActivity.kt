599c1c84a1b9f2d1a503264a5863703119932589
Renad Omar <reomqu@gmail.com>
2021-12-29 16:33:29 +0300

    Init Commit

---
+++ app/src/main/java/com/renad/sudoku/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.renad.sudoku.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.renad.sudoku.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
